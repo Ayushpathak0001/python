dc483e898b884943b01a2b193f5e87b51be5cf10
Ayushpathak0001 <ayushraj132007@gmail.com>
2025-12-11 19:30:48 +0530

    inputandprint

---
+++ string.py
@@ -0,0 +1,10 @@
+student_name=input("enter student name : ")
+student_department=input("Hii enter departmen : ")
+student_course=input("Hii enter student course : ")
+student_rollno=input("Hii enter student's rollno : ")
+print("Student name is : ",student_name)
+print("Student's Department is:",student_department)
+print("Student's course is : ",student_course)
+print("Student's roll no is : ",student_rollno)
+
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+my_name='AYUSH'
+print(my_name)
