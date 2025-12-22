a9ca426209a3ecf1dc7795b660dd6ec7c335fdd9
Ayushpathak0001 <ayushraj132007@gmail.com>
2025-12-22 15:13:03 +0530

    length

---
+++ lengthofstring.py
@@ -0,0 +1,13 @@
+str1="my name is Ayush "
+str2="\nmy college is Shobhit university"
+length1=len(str1)
+length2=len(str2)
+#concatenate
+final_string=str1+str2
+length3=len(final_string)
+print(str1)
+print(str2)
+print(length1)
+print(length2)
+print(final_string)
+print(length3)
