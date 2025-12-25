2942c29a093f6eeea29896e55bf05c148a5e6632
Ayushpathak0001 <ayushraj132007@gmail.com>
2025-12-25 21:50:42 +0530

    armstrong

---
+++ armstrong.py
@@ -0,0 +1,15 @@
+num = int(input("Enter a number: "))
+
+digits = len(str(num))
+temp = num
+total = 0
+
+while temp > 0:
+    digit = temp % 10
+    total += digit ** digits
+    temp //= 10
+
+if total == num:
+    print(num, "is an Armstrong number")
+else:
+    print(num, "is not an Armstrong number")
