fc010440d144285059e7fd37643d7f7da21befc4
Ayushpathak0001 <ayushraj132007@gmail.com>
2026-01-12 12:19:31 +0530

    greatestofthreenumbers

---
+++ ifelifELSE.py/greatestofthreeNUMBERS.py
@@ -0,0 +1,9 @@
+num1=int(input("ENTER FIRST NUMBER :"))
+num2=int(input("ENTER SECOND NUMBER :"))
+num3=int(input("ENTER third NUMBER :"))
+if(num1>num2 and num1>num3):
+    print("NUMBER 1 IS LARGEST WHICH IS : ",num1)
+elif(num2>num1 and num2>num3):
+    print("NUMBER 2 IS LARGEST WHICH IS :",num2)
+elif(num3>num1 and num3>num2):
+    print("NUMBER 3 IS LARGEST WHICH IS : ",num3)
---
+++ ifelifELSE.py/oddoreven.py
@@ -0,0 +1,5 @@
+a=int(input("ENTER  NUMBER "))
+if(a%2==0):
+    print("NUMBER IS EVEN")
+else:
+    print("NUMBER IS ODD")
