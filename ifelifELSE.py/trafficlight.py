8643e83bd883f657610289d25fcbc0a4d54f184f
Ayushpathak0001 <ayushraj132007@gmail.com>
2026-01-12 11:50:51 +0530

    examgrades

---
+++ ifelifELSE.py/trafficlight.py
@@ -0,0 +1,10 @@
+light=input("enter light : ")
+
+if(light=="red"):
+    print("stop")
+elif(light=="yellow"):
+    print("wait")
+elif(light=="green"):
+    print("go")
+else:
+        print("ligth is broken")
