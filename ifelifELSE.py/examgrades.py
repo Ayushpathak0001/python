590964c6dc433bcf1d7eba33a6932aad52d2eb2e
Ayushpathak0001 <ayushraj132007@gmail.com>
2026-01-12 12:17:24 +0530

    greatestofthreenumbers

--- ifelifELSE.py/examgrades.py
+++ ifelifELSE.py/examgrades.py
@@ -1,9 +1,9 @@
 marks=int(input("enter your marks : "))
 if(marks>=90):
     print("Grade = A")
-elif(marks>=80):
+elif(marks>=80 and marks<90):
     print("Grade = B")
-elif(marks>=70):
+elif(marks>=70 and marks<80):
     print("Grade = C")
 else:
-    print("Grade = D")
+    print("Grade = D" )
