eb028c01339729be889865d2193d745e0ca63018
Ayushpathak0001 <ayushraj132007@gmail.com>
2026-01-16 09:02:42 +0530

    setanditsMETHODS

---
+++ ifelifELSE.py/listmethods.py/dictionary.py/set.py/firstset.py
@@ -0,0 +1,22 @@
+collection1 = {1,2,3,4,5}
+print("\n",collection1)
+
+collection1.add(89)
+collection1.add(87)
+print("\n",collection1)
+
+collection1.remove(87)
+print("\n",collection1)
+
+collection1.pop()
+collection1.pop()
+print("\n",collection1)
+
+collection2 ={3,4,8,9}
+
+print("\n",collection1.union(collection2))
+
+print("\n",collection1.intersection(collection2))
+
+collection3 ={7,7,6,5,4,3}
+print("\n",collection3.clear())
