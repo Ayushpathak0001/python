75186957538932befda5eca9bc9eb67046e1f11f
Ayushpathak0001 <ayushraj132007@gmail.com>
2026-01-17 11:45:52 +0530

    forlooppractice

---
+++ ifelifELSE.py/listmethods.py/dictionary.py/forloops.py/practice1.py
@@ -0,0 +1,25 @@
+nums =[1,2,3,5,6,7,8,9,10]
+for el in nums:
+    print("\n",el,"\n")
+
+no = [ 1,2,3,4,5,6,7,8,9,10]
+for n in no :
+    if(n%2==0):
+        print("\n",n)
+
+        super_heroes =["Iron man","spiderman","captain america","thor","hulk"]
+        for names in super_heroes:
+            print("\n",names)
+        else:
+                print("\n end list \n")
+
+                
+super_heroes =["Iron man","spiderman","captain america","thor","hulk"]
+i =0
+while i<len(super_heroes):
+    i+=1
+for names in super_heroes:
+        if(names =="thor"):
+            print("thor found",i)
+else :
+                ("not found")
