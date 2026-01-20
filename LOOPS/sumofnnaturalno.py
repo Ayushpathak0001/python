e8d76b5419fca5839e4d99c85eb792ba59ede84e
Ayushpathak0001 <ayushraj132007@gmail.com>
2026-01-21 00:19:34 +0530

    sumandfactorial

--- LOOPS/sumofnnaturalno.py
+++ LOOPS/sumofnnaturalno.py
@@ -1,6 +1,7 @@
 n = int(input("enter number : "))
-sum=0
-for i in range (1,n+1):
-    sum+=i
-    print("sum is equal to :",sum)
-   
+sum = 0
+
+for i in range(1, n + 1):
+    sum += i
+
+print("sum is equal to :", sum)
